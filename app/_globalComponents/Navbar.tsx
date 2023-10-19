740e180f084cf9f5af9dd31efe1b1ea39fdf50ce
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-10-19 11:08:25 +0545

    responsive navigation

--- app/_globalComponents/Navbar.tsx
+++ app/_globalComponents/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,44 +11,120 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
   return (
-    <nav className="flex justify-between w-screen h-16 bg-red-50">
-      <div className="relative w-16 h-16">
-        <Image src={"/logo.svg"} fill alt="logo" />
+    <nav className="flex justify-between sm:justify-normal gap-4 sm:gap-56 w-screen h-16 border-b-violet-50">
+      <div className="logoContainer flex items-center">
+        <div className="relative w-12 h-12 pb-4">
+          <Image src={"/logo.svg"} fill alt="logo" />
+        </div>
+        <h2 className="font-bold ">Ghanendra Purush Dhakal</h2>
       </div>
-      <ul className="flex gap-4 h-full items-center">
-        <li>Home </li>
-        <li>
+
+      <div
+        className=" sm:hidden self-end h-full flex items-center"
+        onClick={() => {
+          setShowNavbar(!showNavbar);
+        }}
+      >
+        <Image src={"/hamMenu.png"} width={40} height={40} alt="ham icon" />
+      </div>
+      <ul
+        className={cn(
+          "flex flex-col text-sm font-semibold absolute -left-full px-4 pt-8 sm:pt-0 sm:px-12 top-16  sm:w-fit sm:inset-0 bg-blue-primary w-screen sm:bg-transparent sm:relative sm:flex-row gap-4 h-full sm:items-center transition duration-100 min-w-fit mobileNav",
+          {
+            "left-0": showNavbar,
+          }
+        )}
+      >
+        <li className="hover:text-orange-secondary transition cursor-pointer">
+          Home{" "}
+        </li>
+        <li className="hover:text-orange-secondary transition cursor-pointer">
           {" "}
           <DropdownMenu>
             <DropdownMenuTrigger>Books</DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Books</DropdownMenuLabel>
+              <DropdownMenuLabel> Books</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Iron </DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-
               <DropdownMenuSub>
                 <DropdownMenuSubTrigger>
-                  {/* <UserPlus className="mr-2 h-4 w-4" /> */}
-                  <span>Invite users</span>
+                  <span>Jana Andolan</span>
                 </DropdownMenuSubTrigger>
                 <DropdownMenuPortal>
                   <DropdownMenuSubContent>
                     <DropdownMenuItem>
-                      {/* <Mail className="mr-2 h-4 w-4" /> */}
                       <span>Email</span>
                     </DropdownMenuItem>
                     <DropdownMenuItem>
-                      {/* <MessageSquare className="mr-2 h-4 w-4" /> */}
                       <span>Message</span>
                     </DropdownMenuItem>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem>
-                      {/* <PlusCircle className="mr-2 h-4 w-4" /> */}
+                      <span>More...</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <span>Ironman</span>
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuItem>
+                      <span>Email</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <span>Message</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <span>More...</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <span>Lauhapurush</span>
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuItem>
+                      <span>Email</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <span>Message</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <span>More...</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <span>
+                    Ganeshman Singh <br /> in the Eyes of Legends
+                  </span>
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent>
+                    <DropdownMenuItem>
+                      <span>Email</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <span>Message</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
                       <span>More...</span>
                     </DropdownMenuItem>
                   </DropdownMenuSubContent>
@@ -56,10 +133,40 @@ const Navbar = () => {
             </DropdownMenuContent>
           </DropdownMenu>
         </li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className="hover:text-orange-secondary transition cursor-pointer">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Journalism</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel> Journalism</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem> Newspaper</DropdownMenuItem>
+              <DropdownMenuItem> Articles</DropdownMenuItem>
+              <DropdownMenuItem> Columns</DropdownMenuItem>
+              <DropdownMenuItem> Interviews</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </li>
+        <li className="hover:text-orange-secondary transition cursor-pointer">
+          {" "}
+          <DropdownMenu>
+            <DropdownMenuTrigger>Activities</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel> Activities</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem> Ganeshman Singh Foundation</DropdownMenuItem>
+              <DropdownMenuItem> Bankali Garden</DropdownMenuItem>
+              <DropdownMenuItem> Social Service</DropdownMenuItem>
+              <DropdownMenuItem> Trainings</DropdownMenuItem>
+              <DropdownMenuItem> Spirituality</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </li>
+        <li className="hover:text-orange-secondary transition cursor-pointer">
+          Blogs
+        </li>
+        <li className="hover:text-orange-secondary transition cursor-pointer">
+          Media
+        </li>
       </ul>
     </nav>
   );
