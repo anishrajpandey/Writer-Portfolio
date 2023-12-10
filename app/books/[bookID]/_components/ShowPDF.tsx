727dc669d02100cb28cd6d65cebca645291d13c5
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-12-11 00:35:30 +0545

    test on deployment with a single page

--- app/books/[bookID]/_components/ShowPDF.tsx
+++ app/books/[bookID]/_components/ShowPDF.tsx
@@ -29,7 +29,7 @@ export default function ShowPDF({
     <div className="flex justify-center bg-gray-300 h-screen overflow-y-scroll w-full px-4 md:px-12 md:scale-110">
       <Document
         // file="https://www.africau.edu/images/default/sample.pdf"
-        file={`/books/${Book}/${Chapter}.pdf`}
+        file={`/books/${Book}/1.pdf`}
         onLoadSuccess={onDocumentLoadSuccess}
       >
         {/* {Array.apply(null, Array(numPages))
