cc1bf6d6783d47f6289f698ce332a767480ce45c
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-02 21:25:56 +0545

    removed react-pdf dependancy; instead used iframe to render pdf

--- app/books/[bookID]/_components/ShowPDF.tsx
+++ app/books/[bookID]/_components/ShowPDF.tsx
@@ -25,31 +25,15 @@ export default function ShowPDF({
     return Array.from({ length: n }, (_, index) => index + 1);
   }
   return (
-    <div className="flex justify-center bg-gray-300 h-screen overflow-y-scroll w-full px-4 md:px-12 md:scale-110">
-      <Document
-        // file="https://www.africau.edu/images/default/sample.pdf"
-        file={`/books/${Book}/1.pdf`}
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        {/* {Array.apply(null, Array(numPages))
-          .map((x, i) => i + 1)
-          .map((page) => ( */}
-        <div>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-          <Page
-            pageNumber={pageNumber}
-            renderAnnotationLayer={false}
-            renderTextLayer={false}
-          />
-        </div>
-        {/* ))} */}
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
+    <iframe
+      src="https://firebasestorage.googleapis.com/v0/b/visionventures-b8ed4.appspot.com/o/book-pdf%2FThe%20Ironman%20-%20PressReadyDummy2%20Book%202023.02.01%20SplitPages%20(1).pdf?alt=media&token=a21d7cb4-aacc-4bff-a5ea-41271cf700f2"
+      className="w-full h-screen"
+      // width="4800"
+      // height="800"
+    />
+    // <div className="flex justify-center bg-gray-300 h-screen overflow-y-scroll w-full px-4 md:px-12 md:scale-110">
+
+    // </div>
   );
 }
 ShowPDF.propTypes = {
