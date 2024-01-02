85cda6873b1da77d59fb1023813234102dd215ce
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-02 22:16:42 +0545

    fit width pdf iframe

--- app/books/[bookID]/_components/ShowPDF.tsx
+++ app/books/[bookID]/_components/ShowPDF.tsx
@@ -28,8 +28,6 @@ export default function ShowPDF({
     <iframe
       src="https://firebasestorage.googleapis.com/v0/b/visionventures-b8ed4.appspot.com/o/book-pdf%2FThe%20Ironman%20-%20PressReadyDummy2%20Book%202023.02.01%20SplitPages%20(1).pdf?alt=media&token=a21d7cb4-aacc-4bff-a5ea-41271cf700f2"
       className="w-full h-screen"
-      // width="4800"
-      // height="800"
     />
     // <div className="flex justify-center bg-gray-300 h-screen overflow-y-scroll w-full px-4 md:px-12 md:scale-110">
 
