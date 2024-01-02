66d3d50970d2de45b05b149607fa7a7751fe5540
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-02 22:25:52 +0545

    removed unused react-pdf code

--- app/books/[bookID]/_components/ShowPDF.tsx
+++ app/books/[bookID]/_components/ShowPDF.tsx
@@ -1,13 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Document, Page } from "react-pdf";
-import { pdfjs } from "react-pdf";
-import PropTypes from "prop-types";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
 export default function ShowPDF({
   Book,
   Chapter,
@@ -16,14 +9,13 @@ export default function ShowPDF({
   Chapter: any;
 }) {
   const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
-  function createArrayFrom1ToN(n: any) {
-    return Array.from({ length: n }, (_, index) => index + 1);
-  }
+  // function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+  //   setNumPages(numPages);
+  // }
+  // function createArrayFrom1ToN(n: any) {
+  //   return Array.from({ length: n }, (_, index) => index + 1);
+  // }
   return (
     <iframe
       src="https://firebasestorage.googleapis.com/v0/b/visionventures-b8ed4.appspot.com/o/book-pdf%2FThe%20Ironman%20-%20PressReadyDummy2%20Book%202023.02.01%20SplitPages%20(1).pdf?alt=media&token=a21d7cb4-aacc-4bff-a5ea-41271cf700f2"
@@ -35,7 +27,3 @@ export default function ShowPDF({
     // dskjf
   );
 }
-ShowPDF.propTypes = {
-  Book: PropTypes.string.isRequired,
-  Chapter: PropTypes.string.isRequired,
-};
