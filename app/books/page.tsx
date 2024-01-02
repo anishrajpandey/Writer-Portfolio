940395fd34d690ce0e5a68da7257f6fd92f93093
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-02 22:41:35 +0545

    test deployment

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -84,9 +84,9 @@ function BookCard({
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
+                strokeLinecap="round"
                 strokeLinejoin="round"
-                stroke-width="2"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
