57855ff7ebd78901aa2eb275f02065c9b4909ff2
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-07-17 14:52:58 +0545

    added more articles

--- app/journalism/articles/page.tsx
+++ app/journalism/articles/page.tsx
@@ -12,10 +12,7 @@ const Page = () => {
   const [ArticlesToShow, setArticlesToShow] = useState([
     {
       id: 1,
-      title: "Article Title",
-      url: "https://ganeshmansinghfoundation.org/autobiographyseries/peoples-movement-1990-struggle-for-democracy/",
       imageSource: "/assets/files/Article1.jpg",
-      date: "23rd February 2024",
     },
   ]);
 
@@ -39,19 +36,19 @@ const Page = () => {
     <section className="w-screen md:w-[70vw] 12 mx-auto flex flex-col gap-20 ">
       <Title>Articles</Title>
       <Modal ModalData={ModalData} setModalData={setModalData} />
-      {ArticlesToShow.map(({ id, title, imageSource, date, url }) => (
+      {ArticlesToShow.map(({ id, imageSource }) => (
         <article className="flex flex-col items-center shadow-xl" key={id}>
           <header className="w-full flex gap-5 justify-between items-center pb-2">
-            <h1 className="text-lg md:text-xl text-faded font-semibold ">
+            {/* <h1 className="text-lg md:text-xl text-faded font-semibold ">
               {" "}
               {title}
-            </h1>
-            <h4 className="text-sm md:text-md text-faded-light "> {date} </h4>
-            <a href={url} target="blank">
+            </h1> */}
+            {/* <h4 className="text-sm md:text-md text-faded-light "> {date} </h4> */}
+            {/* <a href={url} target="blank">
               <button className="w-fit px-4 py-2 bg-orange-secondary hover:bg-transparent border-1 border-orange-secondary transition text-white hover:text-faded ">
                 See More
               </button>
-            </a>
+            </a> */}
           </header>
 
           <div className="bg-blue-gray-100 m-0 p-0 h-[1px] w-full "></div>
@@ -61,7 +58,7 @@ const Page = () => {
               href={"#"}
               onClick={() => {
                 console.log("clicked");
-                setModalData({ Title: title, Image: imageSource, show: true });
+                setModalData({ Image: imageSource, show: true });
               }}
             >
               <Image
